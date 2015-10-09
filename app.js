bbf92a964658acb787a9951dd82b924b6bdfdda7
Macky Dieng <diengmacky444@gmail.com>
2015-10-09 12:56:13 +0200

    Ajout de l'encodage d'url et du formulaire de recherche

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var findCategories = function(db, callback) {
     callback(result);
   });
 }
+
 var findSubCategories = function(category,db,callback) {
   var collection = db.collection('items');
    collection.find({categorie:category},{fonctionnalite:1}).toArray(function(err, result) {
@@ -40,19 +41,17 @@ function parser(result,fileName,type,param)
 	items = [];
 	links = [];
 	//console.log(result);return;
+	
 	for(var i=0, j=1; i < result.length; i++, j++) {
 		if (type=="category") {
 			items[i] = {"name":result[i]};
 			links[i] = {"source":0,"target":j};
 		}
 		else if (type=="subCategory") {
-			//console.log(result);return;
 			items[i] = {"name":result[i].fonctionnalite[0]};
 			links[i] = {"source":0,"target":j};
 		} else {
 			var sfw_tab = result[i].logiciel_libre_linux;
-			//console.log(sfw_tab);
-			//console.log(result.length);
 			for (var k=0, l=1;  k < sfw_tab.length; k++,l++) {
 				items[k] = {"name":sfw_tab[k]};
 				links[k] = {"source":0,"target":l};
@@ -62,7 +61,8 @@ function parser(result,fileName,type,param)
 	if (type=="category") {
 		items.unshift({"name":"Catégorie"});
 	} else if(type=="subCategory") {
-		items.unshift({"name":param});
+		console.log(items);
+		if (items.length > 0 ) items.unshift({"name":param});
 	} else {
 		items.unshift({"name":param});
 	}
@@ -88,23 +88,27 @@ function loadDataMongo(type, param)
 		console.log("Connected correctly to server");
 		if(type=="category") {
 			findCategories(db, function(result) {
-				//console.log(result);
+				var res = result.length > 0 ? true : false;
 				parser(result,'ui/data/categorie.json',"category");
 				db.close();
+				return res;
 			});
 		} else {
 			if (!isSubCategoryLoaded) {
 				findSubCategories (param, db, function(result) {
-					parser(result,'ui/data/sous_etapes.json',"subCategory",param);
+					var res = result.length > 0 ? true : false;
+					parser(result,'ui/data/categorie.json',"subCategory");
 					db.close();
+					return res;
 				});
 				isSubCategoryLoaded = true;
 			} else {
 				if(isSubCategoryLoaded) {
 					findSoftware(param, db, function(result) {
-						//console.log(result);
-						parser(result,'ui/data/sous_etapes.json',"software",param);
+						var res = result.length > 0 ? true : false;
+						parser(result,'ui/data/categorie.json',"software");
 						db.close();
+						return res;
 					});
 				}
 				isSubCategoryLoaded = false;
@@ -128,9 +132,22 @@ app.use(express.static(__dirname + '/ui/'))
  * */
 .get('/:root', function(req, res) {
 	 
-	loadDataMongo("subStep",req.params.root);
-	//loadDataMongo("software",true,req.params.root);
-    res.render('index.ejs', {});
+	var p  = req.params.root,
+		tab_s = p.split("_"),
+	param = tab_s.length > 0 ? p.replace(/_/g," ") : p;
+	var text_404 = '<h2 style="text-align:center;margin-top:40px;margin:auro;" class="well well-lg">Oups ! il semble que la donnée que vous cherchez n\'existe pas.</h2>';
+	loadDataMongo("subStep",param) ? res.render('index.ejs', {}) :  res.status(404).send(text_404);
+})
+/*Recherche d'une sous-catégorie donnée*/
+.post('/find', urlencodedParser, function(req, res) {
+    if (req.body.term != '') {
+        console.log(req.body.term);
+        res.redirect('/'+req.body.term);
+    }
+    
 });
-
+app.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
 app.listen(8080);
