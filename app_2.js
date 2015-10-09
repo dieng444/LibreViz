da89180e67ac3c32d3bb43351360aa533dc3b0e1
Badacadabra <baptiste.vannesson@gmail.com>
2015-10-09 02:25:36 +0200

    Ajout de la bibliothèque D3-tip et ajout de fonctionnalités dans la sidebar

--- app_2.js
+++ app_2.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var session = require('cookie-session'); 
+var session = require('cookie-session');
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var MongoClient = require('mongodb').MongoClient;
@@ -14,6 +14,7 @@ var findCategories = function(db, callback) {
     callback(result);
   });
 }
+
 var findSubCategories = function(category,db,callback) {
   var collection = db.collection('items');
    collection.find({categorie:category},{fonctionnalite:1}).toArray(function(err, result) {
@@ -30,107 +31,128 @@ var url = 'mongodb://localhost:27017/crawlingproject';
 var items;
 var links;
 var subCategories = [];
-
+var res = false;
 /**
- * Fonction permettant de reformater 
+ * Fonction permettant de reformater
  * le json provenant de la base
  * */
 function parser(result,fileName,type,param)
 {
-	items = [];
-	links = [];
-	//console.log(result);return;
-	for(var i=0, j=1; i < result.length; i++, j++) {
-		if (type=="category") {
-			items[i] = {"name":result[i]};
-			links[i] = {"source":0,"target":j};
-		}
-		else if (type=="subCategory") {
-			//console.log(result);return;
-			items[i] = {"name":result[i].fonctionnalite[0]};
-			links[i] = {"source":0,"target":j};
-		} else {
-			var sfw_tab = result[i].logiciel_libre_linux;
-			//console.log(sfw_tab);
-			//console.log(result.length);
-			for (var k=0, l=1;  k < sfw_tab.length; k++,l++) {
-				items[k] = {"name":sfw_tab[k]};
-				links[k] = {"source":0,"target":l};
-			}
-		}
-	}
-	if (type=="category") {
-		items.unshift({"name":"Catégorie"});
-	} else if(type=="subCategory") {
-		items.unshift({"name":param});
-	} else {
-		items.unshift({"name":param});
-	}
-		
-	var data = {'nodes':items,'links':links};
-	//console.log(data);
-	/**
-	 * Création du fichier json
-	 * */
-	jsonfile.writeFile(fileName, data, {spaces: 2}, function (err) {
-		//console.error(err)
-	});
+    items = [];
+    links = [];
+    //console.log(result);return;
+    //if(result.length > 0) {
+        for(var i=0, j=1; i < result.length; i++, j++) {
+            if (type=="category") {
+                items[i] = {"name":result[i]};
+                links[i] = {"source":0,"target":j};
+            }
+            else if (type=="subCategory") {
+                items[i] = {"name":result[i].fonctionnalite[0]};
+                links[i] = {"source":0,"target":j};
+            } else {
+                var sfw_tab = result[i].logiciel_libre_linux;
+                for (var k=0, l=1;  k < sfw_tab.length; k++,l++) {
+                    items[k] = {"name":sfw_tab[k]};
+                    links[k] = {"source":0,"target":l};
+                }
+            }
+        }
+        if (type=="category") {
+            items.unshift({"name":"Catégorie"});
+        } else if(type=="subCategory") {
+            console.log(items);
+            items.unshift({"name":param});
+        } else {
+            //console.log(items);
+            items.unshift({"name":param});
+        }
+
+        var data = {'nodes':items,'links':links};
+        //console.log(data);
+        /**
+         * Création du fichier json
+         * */
+        jsonfile.writeFile(fileName, data, {spaces: 2}, function (err) {
+            //console.error(err)
+        });
+       // return res = true;
+    //~ } else {
+        //~ return res;
+    //~ }
 }
 /**
  * Permet de charger les données depuis MOngoDB
- * @param param : paramètre reçu de l'url 
+ * @param param : paramètre reçu de l'url
  * **/
  var isSubCategoryLoaded = false;
- 
+
 function loadDataMongo(type, param)
 {
-	MongoClient.connect(url, function(err, db) {
-		console.log("Connected correctly to server");
-		if(type=="category") {
-			findCategories(db, function(result) {
-				//console.log(result);
-				parser(result,'ui/data/categorie.json',"category");
-				db.close();
-			});
-		} else {
-			if (!isSubCategoryLoaded) {
-				findSubCategories (param, db, function(result) {
-					parser(result,'ui/data/sous_etapes.json',"subCategory",param);
-					db.close();
-				});
-				isSubCategoryLoaded = true;
-			} else {
-				if(isSubCategoryLoaded) {
-					findSoftware(param, db, function(result) {
-						//console.log(result);
-						parser(result,'ui/data/sous_etapes.json',"software",param);
-						db.close();
-					});
-				}
-				isSubCategoryLoaded = false;
-			}
-		}
-	});
+    MongoClient.connect(url, function(err, db) {
+        console.log("Connected correctly to server");
+        if(type=="category") {
+            findCategories(db, function(result) {
+                parser(result,'ui/data/categorie.json',"category");
+                db.close();
+            });
+        } else {
+            if (!isSubCategoryLoaded) {
+                findSubCategories (param, db, function(result) {
+                    parser(result,'ui/data/sous_etape.json',"subCategory");
+                    db.close();
+                });
+                isSubCategoryLoaded = true;
+            } else {
+                if(isSubCategoryLoaded) {
+                    findSoftware(param, db, function(result) {
+                        //consoe.log(result);
+                        parser(result,'ui/data/sous_etape.json',"software");
+                        db.close();
+                    });
+                }
+                isSubCategoryLoaded = false;
+            }
+        }
+    });
 }
 /**
  * Spécification du chemin des fichiers static
  * */
 app.use(express.static(__dirname + '/ui/'))
+app.use(express.static(__dirname + '/bower_components/'))
 /**
  * Route d'affichage des catégories
  * */
 .get('/', function(req, res) {
-	loadDataMongo("category");
+    loadDataMongo("category");
     res.render('index_2.ejs', {});
 })
 /**
  * Route d'affichage sous étapes
  * */
 .get('/:root', function(req, res) {
-	 
-	loadDataMongo("subStep",req.params.root);
-	//loadDataMongo("software",true,req.params.root);
-    res.render('index_2.ejs', {});
-});
 
+    var p  = req.params.root,
+        tab_s = p.split("_"),
+    param = tab_s.length > 0 ? p.replace(/_/g," ") : p;
+    //var text_404 = '<h2 style="text-align:center;margin-top:40px;margin:auro;" class="well well-lg">Oups ! il semble que la donnée que vous cherchez n\'existe pas.</h2>';
+    //console.log(loadDataMongo("subStep", param)+"tptp");
+    //loadDataMongo("subStep", param) ? res.render('index_2.ejs', {}) : res.status(404).send(text_404);
+    loadDataMongo("subStep", param);
+    res.render('index_2.ejs', {});
+
+})
+/*Recherche d'une sous-catégorie donnée*/
+.post('/find', urlencodedParser, function(req, res) {
+    if (req.body.term != '') {
+        console.log(req.body.term);
+        res.redirect('/'+req.body.term);
+    }
+
+});
+//~ app.use(function(req, res, next){
+    //~ res.setHeader('Content-Type', 'text/plain');
+    //~ res.send(404, 'Page introuvable !');
+//~ });
 app.listen(8080);
--- ui/js/loadGraph.js
+++ ui/js/loadGraph.js
@@ -1,5 +1,6 @@
 function loadGraph(dataFile)
 {
+
     var width = 782,// window.innerWidth,
         height = 620;// window.innerHeight;
 
@@ -11,71 +12,94 @@ function loadGraph(dataFile)
         .linkDistance(180)
         .size([width, height]);
 
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .html(function(d) { return "<span>" + d.name + "</span>"; });
 
-    var svg = d3.select("div#screen").append("svg")
+    var svg = d3.select("#screen")
+        .append("svg")
         .attr("width", width)
-        .attr("height", height);
+        .attr("height", height)
+        .call(tip);
 
     d3.json(dataFile, function(error, graph) {
-    if (error) throw error;
+      if (error) throw error;
 
-    force
-        .nodes(graph.nodes)
-        .links(graph.links)
-        .start();
+      force
+          .nodes(graph.nodes)
+          .links(graph.links)
+          .start();
 
-    //console.log(graph.links);return;
-    var link = svg.selectAll(".link")
-        .data(graph.links)
+     //console.log(graph.links);return;
+      var link = svg.selectAll(".link")
+          .data(graph.links)
         .enter().append("line")
-        .attr("class", "link")
+          .attr("class", "link")
 
-    var gnodes = svg.selectAll('g.gnode')
-        .data(graph.nodes)
-        .enter()
-        .append('g')
-        .classed('gnode', true);
+      var gnodes = svg.selectAll('g.gnode')
+          .data(graph.nodes)
+          .enter()
+          .append('g')
+          .classed('gnode', true)
 
-    /*var foreignObject = gnodes.append("foreignObject")
-        //.data(graph.nodes)
-        .attr("width", 100)
-        .attr("height", 100);*/
+      /*var foreignObject = gnodes.append("foreignObject")
+          //.data(graph.nodes)
+          .attr("width", 100)
+          .attr("height", 100);*/
 
-    //var a = foreignObject.append('');
-        //.attr('href',"/"+function(d) { return d.name; })
-        //.text(function(d) { return d.name; });
+      //var a = foreignObject.append('');
+          //.attr('href',"/"+function(d) { return d.name; })
+           //.text(function(d) { return d.name; });
 
-    var node = gnodes.append("circle")
-        .attr("class", "node")
-        .attr("r", 35)
-        .on("click", function(d) { location.href = "/"+d.name;})
-        // .style("fill", function(d) { return color(d.group); })
-        .call(force.drag);
+      var node = gnodes.append("circle")
+          .attr("class", "node")
+          .attr("r", 35)
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide)
+          .on("click", function(d) {
+                //var f_url = d.name.replace(/è|é|ê|ë/,"e"),
+                //var f_url = d.name.replace(/'/g,'');
+                //var s_url = d.name.replace(/" "/g,"_");
+                //location.href = "/"+d.name.trim();
+                //return;
+                //À voir pour SIG et les autres pour l'espace qui se trouve devant
+                var cleaned_url = d.name.trim();
+                var tab_s = cleaned_url.split(" "),
+                    url = " ";
+                if (tab_s.length > 1) {
+                    for (var i=0; i < tab_s.length; i++) {
+                        url += tab_s[i]+"_";
+                    }
+                    location.href = "/"+ url.substr(0, url.length - 1).trim();
+                } else
+                    location.href = "/"+cleaned_url;
+          ;})
+          // .style("fill", function(d) { return color(d.group); })
+          .call(force.drag);
 
-    var labels = gnodes.append("text")
-        .attr("x", 0)
-        .attr("y", -39)
-        .on("click", function(d) { location.href = "/"+d.name})
-        .text(function(d) { return d.name; });
+      var labels = gnodes.append("text")
+          .attr("x", 0)
+          .attr("y", -39)
+          .text(function(d) { return d.name; });
 
     //~ var images = gnodes.append("image")
-    //~ .attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Wilber-gimp.png/96px-Wilber-gimp.png")
-    //~ .attr("x", 0)
-    //~ .attr("y", 0)
-    //~ .attr("width", 80)
-    //~ .attr("height", 80)
-    //~ .on("click", function(d) {alert(d.name);});
+      //~ .attr("xlink:href", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Wilber-gimp.png/96px-Wilber-gimp.png")
+      //~ .attr("x", 0)
+      //~ .attr("y", 0)
+      //~ .attr("width", 80)
+      //~ .attr("height", 80)
+      //~ .on("click", function(d) {alert(d.name);});
 
-    force.on("tick", function() {
+      force.on("tick", function() {
         link.attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
 
-        gnodes.attr("transform", function(d) {
+          gnodes.attr("transform", function(d) {
             return 'translate(' + [d.x, d.y] + ')';
-        });
-    });
+          });
+      });
     });
 }
 //Appel à la function
@@ -83,4 +107,4 @@ function loadGraph(dataFile)
 if (location.pathname=="/") {
     loadGraph("data/categorie.json");
 } else
-    loadGraph("data/sous_etapes.json");
+    loadGraph("data/sous_etape.json");
